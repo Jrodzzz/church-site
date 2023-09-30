d90a7ed3311984604a45f1b535349b16d51f66b0
Jrodzzz <145725972+Jrodzzz@users.noreply.github.com>
2023-09-30 00:04:28 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const faders = document.querySelectorAll('.fade-in');
 
 const appearOptions = {
     threshold: 1,
-    rootMargin: "0px 0px -100px 0px"
+    rootMargin: "0px 0px 80px 0px"
 };
 
 const appearOnScroll = new IntersectionObserver
